bd183ec97240c13b87d42b24ce625577af8c5a29
ROHIT KUMAR <kumar111222rohit@gmail.com>
2023-10-05 10:12:23 +0000

    setup boiler plate

--- questions-and-answers/src/pages/qna.tsx
+++ questions-and-answers/src/pages/qna.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
 import { QuestionProvider } from '@/app/module/QnA/QuestionContext/QuestionContext';
 import { QnA } from '@/app/module/QnA/components/QnASection/QnA';
 import { questionStore } from '@/app/store/questionStore';
-import { Provider } from 'react-redux';
 import '../../i18';
 
 export default function QnAPage() {
